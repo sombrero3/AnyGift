00511f35935f2f0d5ee132edbcccee016f1ec0f9
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-12-29 16:29:41 +0200

    adding activity

---
+++ app/src/main/java/com/example/anygift/feed.java
@@ -0,0 +1,14 @@
+package com.example.anygift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
