81b02a6e8b59d6c2d589263170c26c710b8edd32
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-01-19 11:37:29 +0200

    adding menu to main activty

--- app/src/main/java/com/example/anygift/MainActivity.java
+++ app/src/main/java/com/example/anygift/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.anygift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navCtr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavHost navHost= (NavHost) getSupportFragmentManager().findFragmentById(R.id.main_navhost);
+        navCtr=navHost.getNavController();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.base_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (!super.onOptionsItemSelected(item)) {
+            switch(item.getItemId()){
+                case R.id.menu_profile:{
+                    navCtr.navigate(R.id.action_global_userProfileFragment);
+                }
+                case R.id.menu_feed:{
+                    navCtr.navigate(R.id.action_global_cardsDetailsFragment);
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
 }
