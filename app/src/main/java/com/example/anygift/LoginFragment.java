efe3b294a2ee3dc7013ae9901fbd3b957a4e011f
gil_shukrun <gili.shuk2@gmail.com>
2022-01-19 22:06:13 +0200

    Sign Up and Login Fragments

--- app/src/main/java/com/example/anygift/LoginFragment.java
+++ app/src/main/java/com/example/anygift/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.anygift;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.textfield.TextInputEditText;
+
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button signIn_btn;
+    Button signUp_btn;
+    Button forgotP_btn;
+    TextInputEditText email;
+    TextInputEditText password;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        signIn_btn=view.findViewById(R.id.Login_signIn_btn);
+        signUp_btn=view.findViewById(R.id.Login_btn_signUp);
+        forgotP_btn=view.findViewById(R.id.Login_btn_forgotp);
+        email=view.findViewById(R.id.Login_email_input);
+        password=view.findViewById(R.id.Login_password_input);
+        signIn_btn.setTypeface(Typeface.SANS_SERIF);
+        signUp_btn.setTypeface(Typeface.SANS_SERIF);
+        forgotP_btn.setTypeface(Typeface.SANS_SERIF);
+        return view;
     }
 }
+
+/*
+
+nee to implement with firebase that the user is in the users database.
+ */
