f33db27c7d15c128b15cddf83c9cce8c5cd51e9b
Shir Fogel <shirfogel@cs.colman.ac.il>
2022-01-29 17:22:27 +0200

    adding details in details fragment

--- app/src/main/java/com/example/anygift/CardsDetailsFragment.java
+++ app/src/main/java/com/example/anygift/CardsDetailsFragment.java
@@ -3,19 +3,56 @@ package com.example.anygift;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.anygift.model.GiftCard;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CardsDetailsFragment extends Fragment {
-
   View view;
+    FeedViewModel viewModel;
+    UserViewModel userViewModel;
+    private TextView name;
+  private TextView value;
+  private TextView buyAt;
+  private Button editBtn;
+  private Button stores;
+
+    public CardsDetailsFragment() {
+
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_cards_details, container, false);
+        viewModel = new ViewModelProvider(this).get(FeedViewModel.class);
+        userViewModel=new ViewModelProvider(this).get(UserViewModel.class);
+        int giftCardId =CardsDetailsFragmentArgs.fromBundle(getArguments()).getGiftCardId();
+
+     //toDo:checking
+        name=view.findViewById(R.id.details_username_tv);
+        name.setText(userViewModel.getUserName());
+
+        value=view.findViewById(R.id.details_value_tv);
+        buyAt=view.findViewById(R.id.details_buyatval_tv);
+        editBtn=view.findViewById(R.id.details_edit_btn);
+        stores=view.findViewById(R.id.details_stores_btn);
+
+        //toDo:checking
+        if(viewModel.getList().getValue().get(giftCardId).getOwnerEmail().equals(FirebaseAuth.getInstance().getUid())) {
+            editBtn.setVisibility(View.VISIBLE);
+        }
+
+        GiftCard giftCard = viewModel.getList().getValue().get(giftCardId);
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/anygift/UserProfileFragment.java
+++ app/src/main/java/com/example/anygift/UserProfileFragment.java
@@ -9,21 +9,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class UserProfileFragment extends Fragment {
 
     View view;
     Button editbtn;
+    TextView name,phone,email, cardsNumber;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_user_profile, container, false);
         editbtn=view.findViewById(R.id.profileF_editInfoBtn);
+        name=view.findViewById(R.id.profileF_name);
+        phone=view.findViewById(R.id.profileF_phone);
+        email=view.findViewById(R.id.profileF_mail);
+        cardsNumber=view.findViewById(R.id.profileF_cards);
         editbtn.setOnClickListener((v)->{
             Navigation.findNavController(v).navigate(R.id.action_userProfileFragment_to_editProfileFragment);
         });
         return view;
     }
+
 }
