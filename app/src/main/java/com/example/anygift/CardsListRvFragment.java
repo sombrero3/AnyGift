3a7d1d0a39385a365945461dc8d963e61859cc6c
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-02-09 13:26:29 +0200

    fix merge problemes

---
+++ app/src/main/java/com/example/anygift/CardsListRvFragment.java
@@ -0,0 +1,111 @@
+package com.example.anygift;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+
+
+import com.example.anygift.model.GiftCard;
+import com.example.anygift.model.Model;
+
+
+public class CardsListRvFragment extends Fragment {
+    List<GiftCard> cards;
+    EditText cardEt;
+    ImageView cardIv;
+
+@Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cards_list_rv, container, false);
+       cards= Model.instance.getAllGiftCard().getValue();
+
+        RecyclerView list = view.findViewById(R.id.cards_list_rv);
+        list.setHasFixedSize(true);
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+        MyAdapter adapter = new MyAdapter();
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                String cardName = cards.get(position).getCardName();
+                Log.d("TAG","user's row clicked: " + cardName);
+                //Navigation.findNavController(v).navigate(StudentListRvFragmentDirections.actionStudentListRvFragmentToStudentDetailsFragment(stId));
+
+            }
+        });
+
+
+        return view;
+
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView cardVal;
+        ImageView cardImage;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            cardVal = itemView.findViewById(R.id.cards_list_row_et_vlaue);
+            cardImage = itemView.findViewById(R.id.cards_list_row_iv);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    listener.onItemClick(v,pos);
+                }
+            });
+
+        }
+    }
+    interface OnItemClickListener{
+        void onItemClick(View v,int position);
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.cards_list_row,parent,false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            GiftCard card = cards.get(position);
+            holder.cardVal.setText(String.valueOf(card.getValue()));
+
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return cards.size();
+        }
+    }
+}
