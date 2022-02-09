ddf66b647532fde3435ceaf3f2a789bf47b7a3d3
Shir Fogel <shirfogel@cs.colman.ac.il>
2022-02-09 15:37:59 +0200

    login validation

--- app/src/main/java/com/example/anygift/LoginFragment.java
+++ app/src/main/java/com/example/anygift/LoginFragment.java
@@ -8,12 +8,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.anygift.model.Model;
@@ -38,6 +40,8 @@ public class LoginFragment extends Fragment {
 
     View view;
     Snackbar mySnackbar;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressBar pb;
 
     Button signIn_btn;
     Button signUp_btn;
@@ -58,6 +62,8 @@ public class LoginFragment extends Fragment {
         forgotP_btn=view.findViewById(R.id.Login_btn_forgotp);
         email=view.findViewById(R.id.Login_email_input);
         password=view.findViewById(R.id.Login_password_input);
+        pb=view.findViewById(R.id.login_prob);
+        pb.setVisibility(View.INVISIBLE);
 
         signIn_btn.setTypeface(Typeface.SANS_SERIF);
         signUp_btn.setTypeface(Typeface.SANS_SERIF);
@@ -78,6 +84,20 @@ public class LoginFragment extends Fragment {
 
         email_user=email.getText().toString();
         password_user=password.getText().toString();
+        if(TextUtils.isEmpty(email_user) && email_user.matches(emailPattern))
+        {
+            email.setError("please enter  correct   email");
+            return;
+        }
+        if(TextUtils.isEmpty(password_user))
+        {
+            password.setError("please enter correct  password");
+            return;
+        }
+
+        pb.setVisibility(View.VISIBLE);
+
+        //authenticate the user
         mAuth.signInWithEmailAndPassword(email_user,password_user).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -85,13 +105,16 @@ public class LoginFragment extends Fragment {
                     signIn_btn.setEnabled(false);
                     signUp_btn.setEnabled(false);
                     forgotP_btn.setEnabled(false);
+                    pb.setVisibility(View.INVISIBLE);
                     mySnackbar = Snackbar.make(view, "Login successful :)", BaseTransientBottomBar.LENGTH_LONG);
                     mySnackbar.show();
                     Log.d("TAG","login successful");
                     Navigation.findNavController(view).navigate(LoginFragmentDirections.actionLoginFragmentToUserProfileFragment());
                 }
                 else
-                    Log.d("TAG","Login failed");
+                    //Log.d("TAG","Login failed");
+                    Toast.makeText(getContext(),"Error! "+ task.getException().getMessage() ,Toast.LENGTH_LONG).show();
+                pb.setVisibility(View.INVISIBLE);
             }
         });
     }
