ba18c2c141346a4cc7a47686325de6757ecf0b28
omert <tatiomer@outlook.co.il>
2022-02-10 18:58:57 +0200

    adding checks

--- app/src/main/java/com/example/anygift/AddCardFragment.java
+++ app/src/main/java/com/example/anygift/AddCardFragment.java
@@ -24,11 +24,14 @@ import android.widget.TextView;
 
 import com.example.anygift.model.GiftCard;
 import com.example.anygift.model.Model;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.text.DateFormat;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -36,21 +39,19 @@ import java.util.Date;
 public class AddCardFragment extends Fragment {
     private static final int REQUEST_CAMERA = 1;
     TextInputEditText cardValue;
-    TextInputEditText cardExpDay,cardExpMonth,cardExpYear;
+    TextInputEditText cardExpDay, cardExpMonth, cardExpYear;
     TextInputEditText cardAskingValue;
     TextInputEditText cardNumber;
     ImageButton uploadPicButton;
     Button addCardButton;
     Bitmap giftCardBitmap;
     View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_add_card, container, false);
-
-        //giftCardInTitle = view.findViewById(R.id.add_card_giftcard_iv);
-        //title = view.findViewById(R.id.add_header_tv);
         cardNumber = view.findViewById(R.id.add_card_number);
         cardValue = view.findViewById(R.id.add_card_value);
         cardExpDay = view.findViewById(R.id.add_card_exp_day);
@@ -59,7 +60,7 @@ public class AddCardFragment extends Fragment {
         cardAskingValue = view.findViewById(R.id.add_card_exp_value);
 
 
-       // uploadPicText = view.findViewById(R.id.add_take_picture_tv);
+        // uploadPicText = view.findViewById(R.id.add_take_picture_tv);
         uploadPicButton = view.findViewById(R.id.add_camera_bt);
         addCardButton = view.findViewById(R.id.add_upload_bt);
         addCardButton.setOnClickListener(new View.OnClickListener() {
@@ -74,81 +75,96 @@ public class AddCardFragment extends Fragment {
 
         return view;
     }
-    private void openCam () {
+
+    private void openCam() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, REQUEST_CAMERA);
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CAMERA){
-            if (resultCode == Activity.RESULT_OK){
+        if (requestCode == REQUEST_CAMERA) {
+            if (resultCode == Activity.RESULT_OK) {
                 Bundle extras = data.getExtras();
                 giftCardBitmap = (Bitmap) extras.get("data");
             }
         }
     }
-    boolean validateInputDates(String year, String mo, String day){
-        boolean error=false;
-        if (!isInt(year)){
-            cardExpYear.setError("Year Is not Valid");
-            error=true;
-        }
-        if (!isInt(mo)){
-            cardExpMonth.setError("Month Is not valid");
-            error=true;
-        }
-        if (!isInt(day)){
-            cardExpDay.setError("Day Is not valid");
-            error=true;
-        }
-        return error;
+
+    void popMsg(String Msg) {
+        Snackbar mySnackbar = Snackbar.make(view, Msg, BaseTransientBottomBar.LENGTH_LONG);
+        mySnackbar.show();
+
     }
 
-    private void upload(){
-        boolean error=false;
+    void enableButtons() {
+        addCardButton.setEnabled(true);
+        uploadPicButton.setEnabled(true);
+    }
+
+    void disableButtons() {
         addCardButton.setEnabled(false);
         uploadPicButton.setEnabled(false);
+
+    }
+
+    static boolean isLegalDate(String s) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setLenient(false);
+        return sdf.parse(s, new ParsePosition(0)) != null;
+    }
+
+
+
+    void logging(String CardValue, String CardAskingValue, String CardNumber) {
+        Log.d("TAG", CardValue);
+        Log.d("TAG", CardAskingValue);
+        Log.d("TAG", CardNumber);
+    }
+
+    private void upload() {
+        boolean error = false;
+        String errorMsg = "";
+        disableButtons();
         String CardValue = cardValue.getText().toString();
         String CardAskingValue = cardAskingValue.getText().toString();
         String CardNumber = cardNumber.getText().toString();
-
         String day = cardExpDay.getText().toString();
         String mo = cardExpMonth.getText().toString();
         String year = cardExpYear.getText().toString();
-        error = validateInputDates(year,mo,day);
-        if (error) return;
-        int dayI=Integer.parseInt(day);
-        int moI=Integer.parseInt(mo);
-        if(dayI<1||dayI>31||(moI==2&&dayI>29)){
-            cardExpDay.setError("Wrong day");
-            cardExpMonth.setError("Wrong month");
+        String dateFormatted = year + "-" + mo + "-" + day;
+        if (!isLegalDate(dateFormatted)){
+            error=true;
+            errorMsg += "date: " + dateFormatted + " is Invalid!\n";
+        }
+        if (giftCardBitmap == null) {
+            error = true;
+            errorMsg += "GiftCard Picture Is Missing!";
+        }
+
+        if (error) {
+            popMsg(errorMsg);
+            enableButtons();
             return;
         }
 
-        String date=day+"/"+mo+"/"+year;
-        Log.d("TAG",CardValue);
-        Log.d("TAG",CardAskingValue);
-        Log.d("TAG",CardNumber);
-        Log.d("TAG",date);
+        // if reached here all is good
+        String date = day + "/" + mo + "/" + year;
+        logging(CardValue, CardAskingValue, CardNumber);
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user= auth.getCurrentUser();
-        String emailUser=user.getEmail().toString();
+        FirebaseUser user = auth.getCurrentUser();
+        String emailUser = user.getEmail().toString();
 
-        GiftCard newGiftCard = new GiftCard(CardNumber,Double.parseDouble( CardValue),date,Double.parseDouble( CardAskingValue),emailUser); //todo maybe change double to string
+        GiftCard newGiftCard = new GiftCard(CardNumber, Double.parseDouble(CardValue), date, Double.parseDouble(CardAskingValue), emailUser); //todo maybe change double to string
 
-        if (giftCardBitmap == null){
-            Model.instance.addGiftCard(newGiftCard,()->{
+        Model.instance.saveImage(giftCardBitmap, newGiftCard.getId() + ".jpg", url -> {
+            newGiftCard.setGiftCardImageUrl(url);
+            Model.instance.addGiftCard(newGiftCard, () -> {
                 Navigation.findNavController(view).navigate(R.id.action_global_feedFragment);
+                popMsg("GiftCard Added! :)");
             });
-        }else{
-            Model.instance.saveImage(giftCardBitmap, newGiftCard.getId() + ".jpg", url -> {
-                newGiftCard.setGiftCardImageUrl(url);
-                Model.instance.addGiftCard(newGiftCard,()->{
-//                    Navigation.findNavController(nameEt).navigateUp();
-                });
-            });
-        }
+        });
 
     }
 }
