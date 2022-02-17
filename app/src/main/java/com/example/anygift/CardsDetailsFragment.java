ccc2188513bcdbfdbde7112c982bb36be5eb5817
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-02-17 19:35:59 +0200

    adding other user profile fragmen implementaion

--- app/src/main/java/com/example/anygift/CardsDetailsFragment.java
+++ app/src/main/java/com/example/anygift/CardsDetailsFragment.java
@@ -69,6 +69,14 @@ public class CardsDetailsFragment extends Fragment {
         });
         name = view.findViewById(R.id.details_username_tv);
         name.setText(giftCard.getOwnerEmail());
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(CardsDetailsFragmentDirections.actionCardsDetailsFragmentToUserDetailsFragment(giftCard.getOwnerEmail()));
+
+
+            }
+        });
         value = view.findViewById(R.id.details_giftvalue_tv);
         mapBtn=view.findViewById(R.id.cardDetails_mapBtn);
         String val = Double.toString(giftCard.getValue());
--- app/src/main/java/com/example/anygift/UserDetailsFragment.java
+++ app/src/main/java/com/example/anygift/UserDetailsFragment.java
@@ -1,8 +1,11 @@
 package com.example.anygift;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.anygift.model.User;
+import com.squareup.picasso.Picasso;
 
 
 public class UserDetailsFragment extends Fragment {
@@ -33,8 +39,31 @@ public class UserDetailsFragment extends Fragment {
         name = view.findViewById(R.id.UserprofileF_name);
         phone = view.findViewById(R.id.UserprofileF_phone);
         email = view.findViewById(R.id.UserprofileF_mail);
+        MapBtn=view.findViewById(R.id.UserprofileF_mapBtn);;
         profileImage=view.findViewById(R.id.UserprofileF_imageView);
-
+        String userId = UserDetailsFragmentArgs.fromBundle(getArguments()).getUserId();
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        userViewModel.getUserById(userId, new UserViewModel.GetUserListener() {
+            @Override
+            public void onComplete(User user) {
+                name.setText(user.getName());
+                phone.setText(user.getPhone());
+                email.setText(user.getEmail());
+                if (user.getImageUrl() != null) {
+                    Picasso.get().load(user.getImageUrl()).into(profileImage);
+                    profileImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    profileImage.setClipToOutline(true);
+                }
+                latAndLong=user.getLatAndLong();
+            }
+        });
+        MapBtn.setOnClickListener((v)->{
+            String[] cordinate=latAndLong.split(",");
+            String uri = "http://maps.google.com/maps?q=loc:" + cordinate[0] + "," + cordinate[1];
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+            intent.setPackage("com.google.android.apps.maps");
+            startActivity(intent);
+        });
         return view;
     }
 }
