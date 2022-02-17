99754b1ba4279823e8bd81534d2ec716c5c2e99c
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2022-02-17 16:05:37 +0200

    edit card fix

--- app/src/main/java/com/example/anygift/EditCardDetailsFragment.java
+++ app/src/main/java/com/example/anygift/EditCardDetailsFragment.java
@@ -1,11 +1,25 @@
 package com.example.anygift;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +29,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.anygift.model.GiftCard;
+import com.example.anygift.model.Model;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -25,7 +41,7 @@ public class EditCardDetailsFragment extends Fragment {
     EditText buyAt;
     Button save;
     ImageView giftCardImage;
-    ImageButton uploadPic;
+    ImageButton uploadPicbtn;
     UserViewModel userViewModel;
     MyCardsViewModel viewModel;
     GiftCard giftCard = null;
@@ -47,11 +63,120 @@ public class EditCardDetailsFragment extends Fragment {
                 giftCard = gc;
         }
         value = view.findViewById(R.id.editDetails_giftvalue_tv);
+        value.setText(String.valueOf( giftCard.getValue()));
+
         buyAt = view.findViewById(R.id.editDetails_buyatval_tv);
-        uploadPic = view.findViewById(R.id.take_pic_IB);
+        buyAt.setText(String.valueOf( giftCard.getWantedPrice()));
+
+        uploadPicbtn = view.findViewById(R.id.take_pic_IB);
         save = view.findViewById(R.id.editDetails_save_btn);
         giftCardImage = view.findViewById(R.id.editDetails_giftpic_iv);
-
+        Picasso.get().load(giftCard.getGiftCardImageUrl()).into(giftCardImage);
+        uploadPicbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editImage();
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update();
+            }
+        });
         return view;
     }
+
+
+    private void editImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Choose your profile picture");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        giftCardImage.setImageBitmap(selectedImage);
+                        giftCardImage.setTag("img");
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                giftCardImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                giftCardImage.setTag("img");
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void update(){
+        BitmapDrawable drawable = (BitmapDrawable) giftCardImage.getDrawable();
+        Log.d("BITAG", drawable.toString());
+        Bitmap bitmap = drawable.getBitmap();
+        Model.instance.uploadImage(bitmap,giftCard.getId(),new Model.UploadImageListener(){
+            @Override
+            public void onComplete(String url) {
+                if (url == null) {
+                    displayFailedError();
+                } else {
+                    giftCard.setGiftCardImageUrl(url);
+                    giftCard.setValue(Double.parseDouble(value.getText().toString() ));
+                    giftCard.setWantedPrice(Double.parseDouble(buyAt.getText().toString() ));
+                    Model.instance.addGiftCard(giftCard, () -> {
+                        Navigation.findNavController(view).navigate(R.id.action_global_feedFragment);
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    private void displayFailedError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Operation Failed");
+        builder.setMessage("Saving image failed, please try again later...");
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+
 }
