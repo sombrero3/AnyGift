ae478a1e7c668f691a9978516244b776d6c32309
Carmel <sombrero3@walla.com>
2022-04-15 12:53:45 +0300

    search card date implementation

--- app/src/main/java/com/example/anygift/SearchGiftCardFragment.java
+++ app/src/main/java/com/example/anygift/SearchGiftCardFragment.java
@@ -1,5 +1,8 @@
 package com.example.anygift;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +10,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SearchGiftCardFragment extends Fragment {
+    TextView dateTv;
+    DatePickerDialog.OnDateSetListener dateListener;
+    int year,month,day;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search_gift_card, container, false);
 
+        dateTv = view.findViewById(R.id.search_card_date_tv);
+        Calendar calendar = Calendar.getInstance();
+        year=calendar.get(Calendar.YEAR);
+        month=calendar.get(Calendar.MONTH);
+        day=calendar.get(Calendar.DAY_OF_MONTH);
+        dateTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog_MinWidth, dateListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        dateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+                dateTv.setText(day+"/"+month+"/"+year);
+            }
+        };
         return view;
     }
 }
