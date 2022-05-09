caff9be5c941b7f7ed3df5582648b59abc863384
Carmel <sombrero3@walla.com>
2022-05-09 16:22:17 +0300

    tryyy

--- app/src/main/java/com/example/anygift/feed/TransactionDetailsFragment.java
+++ app/src/main/java/com/example/anygift/feed/TransactionDetailsFragment.java
@@ -10,15 +10,36 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.anygift.R;
+import com.example.anygift.model.Model;
 
 
 public class TransactionDetailsFragment extends Fragment {
     TextView typeTv,numTv,expTv,dealDateTv,sellerTv,buyerTv,costTv,valueAtDealDateTv,reviewTv;
-
+    String tranId;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_transaction_details, container, false);
+        tranId = TransactionDetailsFragmentArgs.fromBundle(getArguments()).getTranId();
+
+        typeTv = view.findViewById(R.id.trans_details_type_tv);
+        numTv = view.findViewById(R.id.trans_details_number_tv);
+        expTv = view.findViewById(R.id.trans_details_exp_date_tv);
+        dealDateTv = view.findViewById(R.id.trans_details_tran_date_tv);
+        sellerTv = view.findViewById(R.id.trans_details_seller_tv);
+        buyerTv = view.findViewById(R.id.trans_details_buyer_tv);
+        costTv = view.findViewById(R.id.trans_details_cost_tv);
+        valueAtDealDateTv = view.findViewById(R.id.trans_details_value_deal_day_tv);
+        reviewTv = view.findViewById(R.id.trans_details_review_tv);
+
+        setUI();
+
+        return view;
+    }
+
+    private void setUI() {
+        //getTranById
+        //getCardById
     }
 }
